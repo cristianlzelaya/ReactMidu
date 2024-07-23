694ad0de276e24d1bfea03708b33713e31158fd1
cristianlzelaya <cristianlzelaya@gmail.com>
2024-07-23 15:45:53 -0300

    Terminando de renderizar los usuarios

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,18 +1,44 @@
 import "./App.css";
 import { useState } from "react";
-import { TwitterFollowCard } from "./TwitterFollowCard";
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Angel",
+    isFollowing: true,
+  },
+  {
+    userName: "Cristianlzelaya",
+    name: "Cristian Leonel Zelaya",
+    isFollowing: false,
+  },
+  {
+    userName: "Pheral",
+    name: "Pablo H.",
+    isFollowing: true,
+  },
+  {
+    userName: "PacoHdez",
+    name: "Paco",
+    isFollowing: true,
+  },
+];
 export function App() {
   return (
-    <section className="App">
-      <TwitterFollowCard userName="midudev" initialIsFollowing={true}>
-        Miguel Angel
-      </TwitterFollowCard>
-      <TwitterFollowCard userName="pheral">Pablo Hernandez</TwitterFollowCard>
-      <TwitterFollowCard userName="cristianlzelaya">
-        Cristian Leonel
-      </TwitterFollowCard>
-
-      <TwitterFollowCard userName="Elonmusk">Don Elon</TwitterFollowCard>
-    </section>
+    <>
+      <section className="App">
+        {users.map(({ userName, name, isFollowing }) => {
+          return (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+          );
+        })}
+      </section>
+    </>
   );
 }
